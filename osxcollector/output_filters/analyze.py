187e82458085066bf5a85b451c71b50f61caa7e1
jsendor <jsendor@yelp.com>
2016-06-20 13:09:50 -0700

    Created HtmlSummaryFilter class to output the analysis summary in HTML format

--- osxcollector/output_filters/analyze.py
+++ osxcollector/output_filters/analyze.py
@@ -52,7 +52,7 @@ class AnalyzeFilter(ChainFilter):
     effect the operations of the next filter.
     """
 
-    def __init__(self, no_opendns=False, no_virustotal=False, no_shadowserver=False, readout=False, **kwargs):
+    def __init__(self, no_opendns=False, no_virustotal=False, no_shadowserver=False, readout=False, html=False, **kwargs):
 
         filter_chain = []
 
@@ -88,7 +88,10 @@ class AnalyzeFilter(ChainFilter):
             filter_chain.append(FirefoxHistoryFilter(**kwargs))
             filter_chain.append(ChromeHistoryFilter(**kwargs))
 
-        filter_chain.append(_VeryReadableOutputFilter(**kwargs))
+        if html:
+            filter_chain.append(_HtmlSummaryFilter(**kwargs))
+        else:
+            filter_chain.append(_VeryReadableOutputFilter(**kwargs))
 
         super(AnalyzeFilter, self).__init__(filter_chain, **kwargs)
 
@@ -114,6 +117,8 @@ class AnalyzeFilter(ChainFilter):
                            help='[OPTIONAL] Output unsigned startup items and kexts.')
         group.add_argument('--show-browser-ext', dest='show_browser_ext', action='store_true', default=False,
                            help='[OPTIONAL] Output the list of installed browser extensions.')
+        group.add_argument('--html', dest='html', action='store_true', default=False,
+                           help='[OPTIONAL] Print summary in HTML format.')
         return parser
 
     @staticmethod
@@ -226,9 +231,9 @@ class _VeryReadableOutputFilter(OutputFilter):
             sys.stdout.write(color)
         try:
             sys.stdout.write(msg.encode("utf-8", errors="ignore"))
-        except UnicodeDecodeError:
-            # TODO: add optional debug information about the error
+        except UnicodeDecodeError as err:
             sys.stdout.write(msg)
+            sys.stderr.write('Unicode decode error: {0}'.format(err))
         if not self._monochrome:
             sys.stdout.write(self.END_COLOR)
 
@@ -398,6 +403,272 @@ class _VeryReadableOutputFilter(OutputFilter):
             self._write('"')
         elif isinstance(val, Number):
             self._write('{0}'.format(val), self.VAL_COLOR)
+
+
+class _HtmlSummaryFilter(OutputFilter):
+
+    def __init__(self, monochrome=False, show_signature_chain=False, show_browser_ext=False, **kwargs):
+        super(_HtmlSummaryFilter, self).__init__(**kwargs)
+        self._vthash = []
+        self._vtdomain = []
+        self._opendns = []
+        self._blacklist = []
+        self._related = []
+        self._signature_chain = []
+        self._extensions = []
+        self._monochrome = monochrome
+        self._show_signature_chain = show_signature_chain
+        self._show_browser_ext = show_browser_ext
+
+        self._add_to_blacklist = []
+
+    def filter_line(self, blob):
+        """Each Line of OSXCollector output will be passed to filter_line.
+
+        The OutputFilter should return the line, either modified or unmodified.
+        The OutputFilter can also choose to return nothing, effectively swallowing the line.
+
+        Args:
+            output_line: A dict
+
+        Returns:
+            A dict or None
+        """
+        if 'osxcollector_vthash' in blob:
+            self._vthash.append(blob)
+
+        if 'osxcollector_vtdomain' in blob:
+            self._vtdomain.append(blob)
+
+        if 'osxcollector_opendns' in blob:
+            self._opendns.append(blob)
+
+        if 'osxcollector_blacklist' in blob:
+            self._blacklist.append(blob)
+
+        if 'osxcollector_related' in blob:
+            self._related.append(blob)
+
+        if self._show_signature_chain:
+            if 'signature_chain' in blob and blob['osxcollector_section'] in ['startup', 'kext']:
+                signature_chain = blob['signature_chain']
+                if not len(signature_chain) or 'Apple Root CA' != signature_chain[-1]:
+                    self._signature_chain.append(blob)
+
+        if self._show_browser_ext:
+            if blob['osxcollector_section'] in ['firefox', 'chrome'] and blob.get('osxcollector_subsection') == 'extensions':
+                self._extensions.append(blob)
+
+        return blob
+
+    def _write(self, text):
+        try:
+            sys.stdout.write(text.encode('utf-8', errors='ignore'))
+        except UnicodeDecodeError as err:
+            sys.stdout.write(text)
+            sys.stderr.write('Unicode decode error: {0}'.format(err))
+
+    def end_of_lines(self):
+        """Called after all lines have been fed to filter_output_line.
+
+        The OutputFilter can do any batch processing on that requires the complete input.
+
+        Returns:
+            An array of dicts (empty array if no lines remain)
+        """
+        self._write('''<html><head><style>
+            body {
+                color: #ffffff;
+                background-color: #36454F;
+            }
+
+            p, h1, h2 {
+                color: #ffff00;
+                font-weight: bold;
+            }
+
+            dt {
+                color: #a020f0;
+            }
+
+            .dd {
+                color: #00ff00;
+            }
+        </style></head><body>''')
+        self._print_header('Very Readable Output Bot')
+        self._print_para('Let\'s see what\'s up with this machine.')
+
+        if len(self._vthash):
+            # <div id="vthash">
+            self._print_header('VirusTotal bad hash hits', level=2)
+            self._print_para('Dang! You\'ve got known malware on this machine. Hope it\'s commodity stuff')
+            self._summarize_blobs(self._vthash)
+            self._print_para('Sheesh! This is why we can\'t have nice things!')
+
+        if len(self._vtdomain):
+            # <div id="vtdomain">
+            self._print_header('VirusTotal bad domain hits', level=2)
+            self._print_para('I see you\'ve been visiting some \'questionable\' sites. If you trust VirusTotal that is.')
+            self._summarize_blobs(self._vtdomain)
+            self._print_para('I hope it was worth it!')
+
+        if len(self._opendns):
+            # <div id="opendns">
+            self._print_header('OpenDNS Investigate hits', level=2)
+            self._print_para('Well, here\'s some domains OpenDNS wouldn\'t recommend.')
+            self._summarize_blobs(self._opendns)
+            self._print_para('You know you shouldn\'t just click every link you see? #truth')
+
+        if len(self._blacklist):
+            # <div id="blacklist">
+            self._print_header('Blacklist hits', level=2)
+            self._print_para('We put stuff on a blacklist for a reason. Mostly so you don\'t do this.')
+            self._summarize_blobs(self._blacklist)
+            self._print_para('SMH')
+
+        if len(self._related):
+            # <div id="related">
+            self._print_header('Related hits', level=2)
+            self._print_para('This whole things started with just a few clues. Now look what I found.')
+            self._summarize_blobs(self._related)
+            self._print_para('Nothing hides from Very Readable Output Bot')
+
+        if len(self._signature_chain):
+            # <div id="signature_chain">
+            self._print_header('Signature chain', level=2)
+            self._print_para('If these binaries were signed by \'Apple Root CA\' I\'d trust them more.')
+            self._summarize_blobs(self._signature_chain)
+            self._print_para('Let\'s just try and stick with some safe software')
+
+        if len(self._extensions):
+            # <div id="extensions">
+            self._print_header('Extensions', level=2)
+            self._print_para('Let\'s see what\'s hiding in the browser, shall we.')
+            self._summarize_blobs(self._extensions)
+            self._print_para('You know these things have privileges galore.')
+
+        if len(self._add_to_blacklist):
+            # <div id="add_to_blacklist">
+            self._add_to_blacklist = list(set(self._add_to_blacklist))
+            self._print_header('Blacklist update suggestions', level=2)
+            self._print_para('If I were you, I\'d probably update my blacklists to include:')
+            for key, val in self._add_to_blacklist:
+                self._summarize_val(key, val)
+            self._print_para('That might just help things, Skippy!')
+
+        self._print_para('Very Readable Output Bot')
+        self._print_para('#kaythanksbye')
+
+        self._write('</body></html>')
+
+        return []
+
+    def _summarize_blobs(self, blobs):
+        for blob in blobs:
+            self._summarize_line(blob)
+
+            add_to_blacklist = False
+
+            if 'osxcollector_vthash' in blob:
+                self._summarize_vthash(blob)
+                add_to_blacklist = True
+
+            if 'osxcollector_vtdomain' in blob:
+                self._summarize_vtdomain(blob)
+
+            if 'osxcollector_opendns' in blob:
+                self._summarize_opendns(blob)
+
+            if 'osxcollector_blacklist' in blob:
+                for key in blob['osxcollector_blacklist'].keys():
+                    self._summarize_val('blacklist-{0}'.format(key), blob['osxcollector_blacklist'][key])
+
+            if 'osxcollector_related' in blob:
+                for key in blob['osxcollector_related'].keys():
+                    self._summarize_val('related-{0}'.format(key), blob['osxcollector_related'][key])
+
+            if 'md5' in blob and '' == blob['md5']:
+                add_to_blacklist = True
+
+            if add_to_blacklist:
+                blacklists = blob.get('osxcollector_blacklist', {})
+                values_on_blacklist = blacklists.get('hashes', [])
+                for key in ['md5', 'sha1', 'sha2']:
+                    val = blob.get(key, '')
+                    if len(val) and val not in values_on_blacklist:
+                        self._add_to_blacklist.append((key, val))
+
+                values_on_blacklist = blacklists.get('domains', [])
+                for domain in blob.get('osxcollector_domains', []):
+                    if domain not in values_on_blacklist:
+                        self._add_to_blacklist.append(('domain', domain))
+
+    def _summarize_line(self, blob):
+        section = blob.get('osxcollector_section')
+        subsection = blob.get('osxcollector_subsection', '')
+
+        self._print_header('{0} {1}'.format(section, subsection), level=3)
+        for key in sorted(blob.keys()):
+            if not key.startswith('osxcollector') and blob.get(key):
+                val = blob.get(key)
+                self._summarize_val(key, val)
+
+    def _summarize_vthash(self, blob):
+        for blob in blob['osxcollector_vthash']:
+            for key in ['positives', 'total', 'scan_date', 'permalink']:
+                val = blob.get(key)
+                self._summarize_val(key, val, 'vthash')
+
+    def _summarize_vtdomain(self, blob):
+        for blob in blob['osxcollector_vtdomain']:
+            for key in ['domain', 'detections']:
+                val = blob.get(key)
+                self._summarize_val(key, val, 'vtdomain')
+
+    def _summarize_opendns(self, blob):
+        for blob in blob['osxcollector_opendns']:
+            for key in ['domain', 'categorization', 'security', 'link']:
+                val = blob.get(key)
+                self._summarize_val(key, val, 'opendns')
+
+    def _summarize_val(self, key, val, prefix=None):
+        self._print_key(key, prefix)
+        self._print_val(val)
+
+    def _print_header(self, text, level=1):
+        self._write('<h{0}>{1}</h{0}>'.format(level, text))
+
+    def _print_para(self, text):
+        self._write('<p>{0}</p>'.format(text))
+
+    def _print_list_item(self, item):
+        self._write('<li>')
+        self._print_val(item)
+        self._write('</li>')
+
+    def _print_key(self, key, prefix):
+        if not prefix:
+            prefix = ''
+        else:
+            prefix += '-'
+
+        self._write('<dt>{0}{1}</dt>'.format(prefix, key))
+
+    def _print_val(self, val):
+        if isinstance(val, list):
+            self._write('<ul>')
+            for v in val:
+                self._print_list_item(v)
+            self._write('</ul>')
+        elif isinstance(val, dict):
+            self._write('<dl>')
+            keys = val.keys()
+            for key in keys:
+                self._write('<dt>{0}</dt>'.format(key))
+                self._print_val(val[key])
+            self._write('</dl>')
+        elif isinstance(val, basestring) or isinstance(val, Number):
+            self._write('<dd>{0}</dd>'.format(val))
 
 
 def main():
