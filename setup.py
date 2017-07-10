4f722df7c06b4ab9fbb04b8173f74f81c03a73d7
Kuba Sendor <jsendor@yelp.com>
2017-07-10 15:13:00 -0700

    Bumping the version to 1.0.18

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="osxcollector_output_filters",
-    version="1.0.17",
+    version="1.0.18",
     author="Yelp Security",
     author_email="opensource@yelp.com",
     description="Filters that process and transform the output of OSXCollector",
